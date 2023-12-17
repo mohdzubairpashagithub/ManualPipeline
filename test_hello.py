bd3ab77a1cd5cddba60bfd680a504ac083fe7d74
mohdzubairpashagithub <153728395+mohdzubairpashagithub@users.noreply.github.com>
2023-12-17 13:30:28 +0300

    Create test_hello.py

---
+++ test_hello.py
@@ -0,0 +1,11 @@
+import unittest
+from hello import hello
+
+
+class TestHello(unittest.TestCase):
+    def test_hello(self):
+        self.assertEqual(hello(), "Hello, Jenkins!")
+
+
+if __name__ == '__main__':
+    unittest.main()
